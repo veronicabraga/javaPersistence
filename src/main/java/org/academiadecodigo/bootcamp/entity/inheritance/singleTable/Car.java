0d55a449eec5dd8329b7ef69cda14ebb016a665c
Veronica Santos <verosbraga@gmail.com>
2020-03-20 18:04:01 +0000

    Single Table Inheritance exercise done and working

---
+++ src/main/java/org/academiadecodigo/bootcamp/entity/inheritance/singleTable/Car.java
@@ -0,0 +1,18 @@
+package org.academiadecodigo.bootcamp.entity.inheritance.singleTable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity(name="CarSingleTable")
+@DiscriminatorValue("car")
+public class Car extends Vehicle {
+    private Integer gears;
+
+    public Integer getGears() {
+        return gears;
+    }
+
+    public void setGears(Integer gears) {
+        this.gears = gears;
+    }
+}
